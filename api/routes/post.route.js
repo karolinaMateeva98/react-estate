9c9f1189df5b5c757a38ea29ae72e141431495b8
“Karolina <karolina.mateeva@mentormate.com>
2024-09-18 17:14:40 +0300

    app completed

---
+++ api/routes/post.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  addPosts,
+  deletePosts,
+  getPost,
+  getPosts,
+  updatePosts,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPosts);
+router.put("/:id", verifyToken, updatePosts);
+router.delete("/:id", verifyToken, deletePosts);
+
+export default router;
